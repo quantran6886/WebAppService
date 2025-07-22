36dce0071036d4b6ba788b8de0c7502884cd3a79
quantran6886 <quan.tm.1954@aptechlearning.edu.vn>
2025-07-22 22:42:56 +0700

    commit

--- WebAppService/Areas/Admin/Controllers/DanhSachNhanSuController.cs
+++ WebAppService/Areas/Admin/Controllers/DanhSachNhanSuController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebAppService.Areas.Admin._Helper;
+using WebAppService.Models.Updates;
 
 namespace WebAppService.Areas.Admin.Controllers
 {
@@ -11,6 +12,37 @@ namespace WebAppService.Areas.Admin.Controllers
         public IActionResult DanhSachNhanSu()
         {
             return View();
+        }
+        AppDbContext db = new AppDbContext();
+
+        [HttpGet]
+        public IActionResult LoadData()
+        {
+            try
+            {
+                var lstChucDanh = db.WebDanhMucHeThongs.Where(c => c.LoaiDanhMuc == "Danh mục chức danh").Select(c => new
+                {
+                    c.IdHeThong,
+                    c.ThuTuTg,
+                    c.TenGoi,
+                    c.GhiChu,
+                });
+
+                return new JsonResult(new
+                {
+                    lstChucDanh,
+                    status = true
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return new JsonResult(new
+                {
+                    message = ex.Message,
+                    status = false
+                });
+            }
         }
     }
 }
