6199d641fe101cc2199e4b84d4992a161f5d8bd2
quantran6886 <quan.tm.1954@aptechlearning.edu.vn>
2025-07-25 11:31:52 +0700

    commit

---
+++ WebAppService/wwwroot/ajax/webappclient.home.js
@@ -0,0 +1,34 @@
+﻿var myClient = {
+
+    init: function () {
+        myClient.Event();
+        myClient.LoadDetail();
+    },
+
+    Event: function () {
+
+    },
+
+    LoadDetail: function () {
+        $.ajax({
+            url: '/Admin/Home/LoadDetail',
+            type: "GET",
+            dataType: "json",
+            data: {
+                CbGiaoDien: "1",
+            },
+            success: function (response) {
+                if (response.status == true) {
+                    var lstData = response.lstData;
+
+                    if (lstData != null && lstData.noiDung != null) {
+                        $("#seo_1").html(lstData.noiDung);
+                    }
+                } else {
+                    toastr.error(response.message, "Lỗi", { showMethod: "slideDown", hideMethod: "slideUp", timeOut: 1000 });
+                }
+            }
+        })
+    },
+}
+myClient.init();
